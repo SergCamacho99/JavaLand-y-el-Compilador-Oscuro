51b6af6cbc4e320d5f9815a84709f78a9f614e70
Dalonso1997 <dalonsoc2501@educantabria.es>
2026-02-04 20:05:38 +0100

    Primeros cambios

---
+++ Poryecto_JavaLand_Equipo2/src/main/java/Interfaces/Combate.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Interfaces;
+
+/**
+ *
+ * @author dam125
+ */
+public interface Combate {
+    
+    //Esta interface va a ser usada por Combate
+    
+}
