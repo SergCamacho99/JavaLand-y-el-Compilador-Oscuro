77ed446b06ee186575f2c65081f41dabfcdc847a
Miguel Martínez <mmartinezr2402@educantabria.es>
2026-02-05 17:19:37 +0100

    uwu

--- Proyecto_JavaLand_Equipo2/src/main/java/Interfaces/CombateInterface.java
+++ Proyecto_JavaLand_Equipo2/src/main/java/Interfaces/CombateInterface.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
-package javaland.interfaces;
+package interfaces;
 
 /**
  *
@@ -10,6 +10,8 @@ package javaland.interfaces;
  */
 public interface CombateInterface {
     //Esta interface va a ser usada por Combate
+    
+    public <T> void turno(T Defensor, T Atacante);
     
     
 }
--- Proyecto_JavaLand_Equipo2/src/main/java/Interfaces/PersonajesInterface.java
+++ Proyecto_JavaLand_Equipo2/src/main/java/Interfaces/PersonajesInterface.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
-package javaland.interfaces;
+package interfaces;
 
 /**
  *
@@ -11,5 +11,6 @@ package javaland.interfaces;
 public interface PersonajesInterface {
     
     //Esta interfaz va a ser usada por Valientes y Monstruos
+    public <T> void atacar(T Personaje); 
     
 }
