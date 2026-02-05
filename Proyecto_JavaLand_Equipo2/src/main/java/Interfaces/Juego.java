1ea31933c27601ce1d15fe2b4d501a21cee83759
Dalonso1997 <dalonsoc2501@educantabria.es>
2026-02-05 16:18:01 +0100

    asas

---
+++ Proyecto_JavaLand_Equipo2/src/main/java/Interfaces/Juego.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package javaland.interfaces;
+
+/**
+ *
+ * @author dam125
+ */
+public interface Juego {
+    
+    public void IniciarJuego();
+    
+    public void creacionOEleccionValiente();
+    
+    public void mostrarMenuPrincipal();
+    
+    public void explorarMapa();
+    
+    public void mostrarEstadoJuego();
+    
+}
---
+++ Proyecto_JavaLand_Equipo2/src/main/java/Interfaces/Personajes.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package javaland.interfaces;
+
+/**
+ *
+ * @author dam125
+ */
+public interface Personajes {
+    
+    //Esta interfaz va a ser usada por Valientes y Monstruos
+    
+}
