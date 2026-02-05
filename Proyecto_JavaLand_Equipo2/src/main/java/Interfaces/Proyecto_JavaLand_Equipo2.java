183a00d22d8d97edb17f7e0cd7427c50102cf198
Dalonso1997 <dalonsoc2501@educantabria.es>
2026-02-05 16:14:47 +0100

    Ahora si (Perdon)

---
+++ Proyecto_JavaLand_Equipo2/src/main/java/Interfaces/Proyecto_JavaLand_Equipo2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Interfaces;
+
+/**
+ *
+ * @author dam125
+ */
+public class Proyecto_JavaLand_Equipo2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
