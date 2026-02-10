ef67693a39825cc645bda7e94b8ae81d0ce530d9
Sergio Camacho Viera <DAM119@iesmhp.local>
2026-02-10 17:29:24 +0100

    mapa

---
+++ Proyecto_JavaLand_Equipo2/src/main/java/Mapa/MainMapa.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Mapa;
+
+/**
+ *
+ * @author DAM119
+ */
+public class MainMapa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Mapa map = new Mapa();
+        map.mostrarMapa();
+    }
+    
+}
