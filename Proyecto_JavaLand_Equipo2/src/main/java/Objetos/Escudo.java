61b7100fdd296e4b0e1f2acea707c4531b4cd032
kbarreto2001 <kbarretor2501@educantabria.es>
2026-02-11 20:02:02 +0100

    k

---
+++ Proyecto_JavaLand_Equipo2/src/main/java/Objetos/Escudo.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Objetos;
+import Personajes.Valiente;
+/**
+ *
+ * @author DAM118
+ */
+
+public class Escudo extends Arma {
+    //Escudo aumenta la defena del valiente
+    public Escudo (int defensa ){
+        super("Escudo", defensa);
+    }
+    
+    @Override
+    public void usar(Valiente Valiente){
+        Valiente.setDefensa(Valiente.getDefensa() + valor);
+        System.out.print("Equipado un escudo. Defensa aumentada");
+    }
+  
+}
