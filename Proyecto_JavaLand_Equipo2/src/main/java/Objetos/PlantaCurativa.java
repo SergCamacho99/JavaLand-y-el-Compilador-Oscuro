a850fc0947386d67a9eaa0ffeb49e66f3f04ca8f
kbarreto2001 <kbarretor2501@educantabria.es>
2026-03-03 19:15:01 +0100

    jjj

--- Proyecto_JavaLand_Equipo2/src/main/java/Objetos/PlantaCurativa.java
+++ Proyecto_JavaLand_Equipo2/src/main/java/Objetos/PlantaCurativa.java
@@ -12,19 +12,30 @@ public class PlantaCurativa extends Consumible{
     // Planta que recupera vida
     
     public PlantaCurativa ( int vida){
-        super("Planta Curativa", vida);
+        super("Planta curativa", vida);
         
     }
     @Override
     public void usar(Valiente valiente){
         /*if(valiente.getVida()>(valiente.getVidaMaxima()-10){
         }else{
-        */
+        
         valiente.setVida(valiente.getVida() + efecto);
         System.out.print("Has usado una planta. Vida recuperada");
-        //}
+        //}*/ 
+        int vidaActual = valiente.getVida(); // obtiene la vida actual del valie 
+        int vidaMaxima = valiente.getVidaMaxima(); //obtener la vida maxima 
+        
+        int recuperacion = (int) (vidaMaxima * 0.25); //25% de la vida 
+        int nuevavida = vidaActual + recuperacion;
+        
+        if ( nuevavida > vidaMaxima) {
+             nuevavida = vidaMaxima;
+             
+        }
+        valiente.setVida(nuevavida);
     }
-
+    
     @Override
     public void equipar() {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
