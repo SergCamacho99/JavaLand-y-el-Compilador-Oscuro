e99e1793b93feb64897d46818cfdab687a0e1791
Dalonso1997 <dalonsoc2501@educantabria.es>
2026-03-04 03:19:25 +0100

    Arreglo de problemas al usar objetos desde el inventario

--- Proyecto_JavaLand_Equipo2/src/main/java/Objetos/Inventario.java
+++ Proyecto_JavaLand_Equipo2/src/main/java/Objetos/Inventario.java
@@ -26,6 +26,7 @@ public class Inventario {
         cantidad = 0;
     }
 //añade un objeto al inventario si hay espacio
+
     public void agregarObjeto(Objeto obj) {
         if (cantidad < Objetos.length) {
             Objetos[cantidad] = obj;
@@ -37,118 +38,125 @@ public class Inventario {
     }
 //metodo mostrar inventario
 //muestra los objetos guardados 
-    public void mostrarInventario() {
-    System.out.println("        +----------------------+");
-    System.out.println("        |      INVENTARIO      |");
-    System.out.println("        +----------------------+");
 
-    if (cantidad == 0) {
-        System.out.println("        | Vacio                |");
-    } else {
-        for (int i = 0; i < cantidad; i++) {
-            // Asegúrate de que el nombre del objeto esté alineado
-            System.out.printf("        | %2d. %s\n", (i + 1), Objetos[i].getnombre());
+    public void mostrarInventario() {
+        System.out.println("        +----------------------+");
+        System.out.println("        |      INVENTARIO      |");
+        System.out.println("        +----------------------+");
+
+        if (cantidad == 0) {
+            System.out.println("        | Vacio                |");
+        } else {
+            for (int i = 0; i < cantidad; i++) {
+                // Asegúrate de que el nombre del objeto esté alineado
+                System.out.printf("        | %2d. %s\n", (i + 1), Objetos[i].getnombre());
+            }
         }
+
+        System.out.println("        +----------------------+");
+        System.out.println("cantidad: " + cantidad);
     }
 
-    System.out.println("        +----------------------+");
-        System.out.println("cantidad: "+cantidad);
-}
     //metodo usar objeto
     //permite usar un objeto buscandolo por su nombre
     public void usarObjeto(String nombre, Valiente valiente) {
-        for (int i = 0; i< cantidad ; i++){
-                
-                if (Objetos[i].getnombre().equalsIgnoreCase(nombre)) {
-                    if(valiente.getVida()==valiente.getVidaMaxima()){
-                        System.out.println("Tu vida esta al maximo, no puedes usar el objeto");
-                    }else{
-                        Objetos[i].usar(valiente);
-                        System.out.println("Objeto usado");
-                        return;
-                    }
-                
+        for (int i = 0; i < cantidad; i++) {
+
+            if (Objetos[i].getnombre().equalsIgnoreCase(nombre)) {
+                if (valiente.getVida() == valiente.getVidaMaxima()) {
+                    System.out.println("Tu vida esta al maximo, no puedes usar el objeto");
+                } else {
+                    Objetos[i].usar(valiente);
+                    System.out.println("Objeto usado");
+                    return;
+                }
+
             }
         }
         System.out.println("No tienes ese objeto");
     }
-    
+
     //verifica que si existe una planta en el iventario
-    public boolean hayPlanta(String nombre){
-        boolean hayPlanta=false;
+    public boolean hayPlanta(String nombre) {
+        boolean hayPlanta = false;
         if (cantidad == 0) {
-            hayPlanta=false;
+            hayPlanta = false;
         } else {
             for (int i = 0; i < cantidad; i++) {
-                if(Objetos[i].getnombre().equalsIgnoreCase(nombre)){
-                    hayPlanta=true; //planta encontrada   
-                  
-                  
+                if (Objetos[i].getnombre().equalsIgnoreCase(nombre)) {
+                    hayPlanta = true; //planta encontrada   
+
                 }
-                
+
             }
         }
-        
+
         if (hayPlanta) {
-            
-            
-            
+
         }
-        
+
         return hayPlanta;
     }
-    
-    public void seleccionarObjeto(int objetoElegido, Valiente valiente){
-        
+
+    public void seleccionarObjeto(int objetoElegido, Valiente valiente) {
+
         objetoElegido -= 1;
-        
-        
-         if (objetoElegido > cantidad){
-            
+
+        if (objetoElegido > cantidad) {
+
             System.out.println("Por favor eleige un objeto existente.");
-            
-        } else if(this.cantidad > 0) {
-        
-        for (int i = 0; i < cantidad; i++) {
-           
-            if (i == objetoElegido){
-                
-                usarObjeto(Objetos[i].getnombre(), valiente);
-                eliminarObjetoInventario(Objetos[i].getnombre());
-                
+
+        } else if (this.cantidad > 0) {
+
+            if (Objetos[objetoElegido] instanceof Espada) {
+                valiente.setArma((Arma) Objetos[objetoElegido]);
+                eliminarObjetoInventario(Objetos[objetoElegido].getnombre());
+            } else if (Objetos[objetoElegido] instanceof Escudo) {
+
+                valiente.setEscudo((Escudo) Objetos[objetoElegido]);
+
+            } else {
+
+                for (int i = 0; i < cantidad; i++) {
+
+                    if (i == objetoElegido) {
+
+                        usarObjeto(Objetos[i].getnombre(), valiente);
+                        eliminarObjetoInventario(Objetos[i].getnombre());
+
+                    }
+
+                }
             }
-            
-        }
         }
     }
-    
-    public void eliminarObjetoInventario(String nombre){
-        
+
+    public void eliminarObjetoInventario(String nombre) {
+
         for (int i = 0; i < cantidad; i++) {
-            
-            if (Objetos[i].getnombre().equals(nombre)){
-                
-                for (int j = i; j < cantidad -1; j++) {
-                    
+
+            if (Objetos[i].getnombre().equals(nombre)) {
+
+                for (int j = i; j < cantidad - 1; j++) {
+
                     Objetos[j] = Objetos[j + 1];
-                    
+
                 }
-                
-                Objetos[cantidad -1] = null;
-                cantidad --;
-                
+
+                Objetos[cantidad - 1] = null;
+                cantidad--;
+
                 return;
             }
-            
+
         }
-    
+
     }
-    
-    
-    public int getCantidad(){
-        
+
+    public int getCantidad() {
+
         return this.cantidad;
-        
+
     }
-    
+
 }
